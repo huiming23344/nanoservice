a88900e7f7ae0b0d3c28908852253f06c1fff079
Huiming <610416981@qq.com>
2024-08-08 15:19:47 +0800

    :tada: init commit

---
+++ time-service/main.go
@@ -0,0 +1,26 @@
+package registry
+
+import (
+	"fmt"
+	routers "github.com/huiming23344/nanoservice/registry/router"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := routers.InitRouter()
+
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Printf("Listen: %s\n", err)
+		}
+	}()
+}
